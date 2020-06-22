4a8dc5c53c074710ddd9cd6807cb5ccafecc74b9
Karen Lei <karenwinnielei@gmail.com>
2020-06-22 13:07:22 -0700

    install redux, react-redux. create reducer

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,44 +1,53 @@
 import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const LoginForm = ( props ) => {
-  const [ login, setLogin ] = useState( {
+const LoginForm = (props) => {
+  const [login, setLogin] = useState({
     username: '',
     password: '',
-  } );
+  });
 
-  const handleChange = ( e ) => {
+  const handleChange = (e) => {
     e.preventDefault();
-    setLogin( {
+    setLogin({
       ...login,
-      [ e.target.name ]: e.target.value,
-    } );
+      [e.target.name]: e.target.value,
+    });
   };
 
-  const handleSubmit = e => {
-    e.preventDefault()
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post()
+      .then(res => {
+        console.log(res)
+        localStorage.setItem('token', res.data.payload)
+        props.history.push('')
+      })
+      .catch((err) => console.log(`Login error: ${err.response}`));
+  };
 
   return (
     <>
-      <form onSubmit={ handleSubmit }>
+      <form onSubmit={handleSubmit}>
         <input
           type='text'
           name='username'
           placeholder='username'
-          value={ props.username }
-          onChange={ handleChange }
+          value={props.username}
+          onChange={handleChange}
         />
         <input
           type='password'
           name='password'
           placeholder='password'
-          value={ props.password }
-          onChange={ handleChange }
+          value={props.password}
+          onChange={handleChange}
         />
         <button>Login</button>
       </form>
     </>
-  )
+  );
 };
 
-export default LoginForm
+export default LoginForm;
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,52 +1,62 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const SignUpForm = props => {
-  const [signUp, setSignUp] = useState({
-    name: '',
-    username: '',
-    password: ''
-  })
+const initialSignUp = {
+  name: '',
+  username: '',
+  password: ''
+}
 
-  const handleChange = e => {
-    e.preventDefault()
+const SignUpForm = (props) => {
+  const [signUp, setSignUp] = useState(initialSignUp);
+
+  const handleChange = (e) => {
+    e.preventDefault();
     setSignUp({
       ...signUp,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  const handleSubmit = e => {
-    e.preventDefault()
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post()
+      .then(res => {
+        console.log(res)
+        setSignUp(initialSignUp)
+      })
+      .catch((err) => console.log(`Sign Up error: ${err.response}`));
+  };
 
   return (
     <>
-    <form onSubmit={handleSubmit}>
-      <input 
-        type = 'text'
-        name = 'name'
-        placeholder = 'name'
-        value = {props.name}
-        onChange = {handleChange}
-      />
-      <input 
-        type = 'text'
-        name = 'username'
-        placeholder = 'username'
-        value = {props.username}
-        onChange = {handleChange}
-      />
-      <input 
-        type = 'text'
-        name = 'password'
-        placeholder = 'password'
-        value = {props.password}
-        onChange = {handleChange}
-      />
-      <button>Sign Up</button>
-    </form>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='name'
+          placeholder='name'
+          value={props.name}
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          name='username'
+          placeholder='username'
+          value={props.username}
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          name='password'
+          placeholder='password'
+          value={props.password}
+          onChange={handleChange}
+        />
+        <button>Sign Up</button>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default SignUpForm
+export default SignUpForm;
