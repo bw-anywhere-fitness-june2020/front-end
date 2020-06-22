6fb3741905610f318651dc50114b7ed6a765da61
Karen Lei <karenwinnielei@gmail.com>
2020-06-22 12:40:59 -0700

    create axiosWIthAuth and PrivateRoute

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import PrivateRoute from './utils/PrivateRoute'
 
 import LoginForm from './components/LoginForm';
+import SignUpForm from './components/SignUpForm'
 
 import './App.css';
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1>Welcome to AnywhereFitness</h1>
-      <LoginForm />
-    </div>
+    <Router>
+      <div className='App'>
+        <h1>Welcome to AnywhereFitness</h1>
+        <Route exact path = '/' component={LoginForm} />
+        <Route exact path = '/signup' component = {SignUpForm} />
+      </div>
+    </Router>
   );
 };
 
