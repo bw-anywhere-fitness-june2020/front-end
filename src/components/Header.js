1672bce400c09a67279b68708139075858207f09
Karen Lei <karenwinnielei@gmail.com>
2020-06-23 12:25:46 -0700

    add logout to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ const Header = () => {
       <NavLink to='/'>Login</NavLink>
       <NavLink to='/signup'>Sign Up</NavLink>
       <NavLink to='/clients'>Clients</NavLink>
+      <NavLink to='/' onClick={() => localStorage.removeItem('token')}>Logout</NavLink>
     </div>
   )
 }
