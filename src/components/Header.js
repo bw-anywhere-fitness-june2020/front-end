8fe58ec138f308450cd2358a64e5156502cb4ef1
Karen Lei <karenwinnielei@gmail.com>
2020-06-24 12:15:48 -0700

    fix linting errors, add key to Card component, and rename button class to className

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <nav>
       <NavLink to='/' className='nav-links'>Login</NavLink>
       <NavLink to='/signup' className='nav-links'>Sign Up</NavLink>
-      <NavLink to='/clients' className='nav-links'>Clients</NavLink>
+      {/* <NavLink to='/clients' className='nav-links'>Clients</NavLink> */}
       <NavLink to='/instructors' className='nav-links'>Instructors</NavLink>
       <NavLink to='/classes' className='nav-links'>Classes</NavLink>
       <NavLink to='/' onClick={() => localStorage.removeItem('token')} className='nav-links'>Logout</NavLink>
